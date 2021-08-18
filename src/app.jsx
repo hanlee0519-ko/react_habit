ec75a1166db6f084b87dce1d826f787ade68a4ba
Han <ahsj22@naver.com>
2021-08-18 13:38:19 -0700

    Change PureComponents From Components

--- src/app.jsx
+++ src/app.jsx
@@ -13,17 +13,24 @@ class App extends Component {
   }
   
   handleIncrement = habit => {
-    const habits = [...this.state.habits];
-    const index = habits.indexOf(habit);
-    habits[index].count++;
+    const habits = this.state.habits.map(item => {
+      if (item.id === habit.id) {
+        return { ...habit, count: habit.count + 1};
+      }
+      return item;
+    });
     this.setState({ habits: habits});
   };
   
   handleDecrement = habit => {
-    const habits = [...this.state.habits];
-    const index = habits.indexOf(habit);
-    const count = habits[index].count - 1;
-    habits[index].count = count < 0 ? 0 : count;
+    const habits = this.state.habits.map(item => {
+      if (item.id === habit.id) {
+        const count = habit.count - 1;
+        return { ...habit, count: count < 0 ? 0 : count };
+      }
+      return item;
+    });
+
     this.setState({ habits: habits });
   };
 
@@ -35,13 +42,15 @@ class App extends Component {
   handleAdd = name => {
     const habits = [...this.state.habits, {id: Date.now(), name: name, count: 0}];
     this.setState({ habits: habits });
-  }
+  };
 
   handleReset = () => {
     const habits = this.state.habits.map(habit => {
-      habit.count = 0;
+      if (habit.count !== 0) {
+        return { ...habit, count: 0 };
+      }
       return habit;
-    })
+    });
     this.setState({ habits: habits });
   };
 
